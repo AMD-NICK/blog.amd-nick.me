75255035665e9a3996175bb48a03f31b4a320827
_AMD_ <amd@default.im>
2022-05-13 19:58:23 +0300

    Откл футер и до конца блог

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -52,7 +52,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       navbar: {
-        title: 'My Site',
+        title: 'Блог блеать',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
@@ -62,60 +62,15 @@ const config = {
             type: 'doc',
             docId: 'intro',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Хуериал',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+        //   {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/AMD-NICK/docusaurustest',
             label: 'GitHub',
             position: 'right',
           },
         ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} Блог @amd_nick, Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
