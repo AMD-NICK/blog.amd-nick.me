caf58b922a04c5c620a660ff870f5c3d58ddd718
_AMD_ <amd@default.im>
2022-05-15 11:05:41 +0300

    update api key

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -97,7 +97,7 @@ const config = {
 			},
 			algolia: {
 				appId: "KCG1D3KM0H",
-				apiKey: "41160d53edbd5216eecfe06fa1ac37be",
+				apiKey: "5cd76a6ffa17bb7b576003f818139788",
 				indexName: "duco",
 			},
 		}),
