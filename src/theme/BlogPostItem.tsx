0941937c94adde0ee6f3dc9b404046834ee108ee
_AMD_ <amd@default.im>
2022-09-24 20:03:14 +0300

    Refactoring BlogPostItem.tsx

--- src/theme/BlogPostItem.tsx
+++ src/theme/BlogPostItem.tsx
@@ -1,67 +1,41 @@
-import React, { useEffect, useRef } from "react";
-import OriginalBlogPostItem from "@theme-original/BlogPostItem";
-// @ts-expect-error internal code
+// based on:
+// https://github.com/IceWhaleTech/ZimaDocs/blob/7ddfa8360dcab96126ca49ce6c2fa87394f20b63/src/theme/DocPaginator/index.js
+// deleted comments front matter option and i18n
+
+import React from "react";
+import BlogPostItem from "@theme-original/BlogPostItem";
 import { useBlogPost } from "@docusaurus/theme-common/internal";
 import { useColorMode } from "@docusaurus/theme-common";
+import Giscus from "@giscus/react";
 
-function BlogPostItem(props) {
+export default function BlogPostItemWrapper(props) {
   const { colorMode } = useColorMode();
   const { isBlogPostPage } = useBlogPost();
-  const widgetTheme = colorMode === "dark" ? "dark_dimmed" : "light";
-  const containerRef = useRef(null);
-
-  useEffect(() => {
-    if (!isBlogPostPage) return;
-
-    const createGiscusEl = () => {
-      const script = document.createElement("script");
-
-      script.src = "https://giscus.app/client.js";
-      script.setAttribute("data-repo", "AMD-NICK/blog.amd-nick.me");
-      script.setAttribute("data-repo-id", "R_kgDOHVBfKA");
-      script.setAttribute("data-category", "Announcements");
-      script.setAttribute("data-category-id", "DIC_kwDOHVBfKM4CRnCI");
-      script.setAttribute("data-mapping", "pathname");
-      script.setAttribute("data-strict", "0");
-      script.setAttribute("data-reactions-enabled", "1");
-      script.setAttribute("data-emit-metadata", "0");
-      script.setAttribute("data-input-position", "top");
-      script.setAttribute("data-theme", widgetTheme);
-      script.setAttribute("data-lang", "ru");
-      script.setAttribute("data-loading", "lazy");
-
-      script.crossOrigin = "anonymous";
-      script.async = true;
-
-      containerRef.current.appendChild(script);
-    };
-
-	function getGiscusEl() {
-		return document.querySelector<HTMLIFrameElement>('iframe.giscus-frame') || createGiscusEl();
-	}
-
-	function sendMessage<T>(message: T) {
-		const iframe = getGiscusEl();
-		if (iframe) {
-			iframe.contentWindow.postMessage({ giscus: message }, 'https://giscus.app');
-		}
-	}
-
-	sendMessage({
-		setConfig: {
-			theme: 'https://giscus.app/themes/' + widgetTheme + '.css',
-		//   reactionsEnabled: false,
-		}
-	});
-
-  }, [widgetTheme]);
+  if (!isBlogPostPage) {
+	return <BlogPostItem {...props} />;
+  }
 
   return (
     <>
-      <OriginalBlogPostItem {...props} />
-      {isBlogPostPage && <div ref={containerRef} />}
+      <BlogPostItem {...props} />
+      {(
+        <div className="docusaurus-mt-lg">
+          <Giscus
+            id="blog_comments"
+            repo="AMD-NICK/blog.amd-nick.me"
+            repoId="R_kgDOHVBfKA"
+            category="Blog comments"
+            categoryId="DIC_kwDOHVBfKM4CRnM9"
+            mapping="pathname"
+            reactionsEnabled="1"
+            emitMetadata="0"
+            inputPosition="top"
+            theme={ colorMode === "dark" ? "dark_dimmed" : "light" }
+            lang={ 'ru' }
+            loading="lazy"
+          />
+        </div>
+      )}
     </>
   );
 }
-
-export default BlogPostItem;
