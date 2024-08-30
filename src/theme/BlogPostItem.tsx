5d5fa1c25d6d83a57c7bd06593af8e4809b98019
_AMD_ <amd@default.im>
2024-08-30 21:02:23 +0300

    Fix комментариев при переходе с discourse 3.0 на 3.5

--- src/theme/BlogPostItem.tsx
+++ src/theme/BlogPostItem.tsx
@@ -4,38 +4,38 @@
 
 import React from "react";
 import BlogPostItem from "@theme-original/BlogPostItem";
-import { useBlogPost } from "@docusaurus/theme-common/internal";
+import { useBlogPost } from '@docusaurus/plugin-content-blog/client';
 import { useColorMode } from "@docusaurus/theme-common";
 import Giscus from "@giscus/react";
 
 export default function BlogPostItemWrapper(props) {
-  const { colorMode } = useColorMode();
-  const { isBlogPostPage } = useBlogPost();
-  if (!isBlogPostPage) {
-	return <BlogPostItem {...props} />;
-  }
+	const { colorMode } = useColorMode();
+	const { isBlogPostPage } = useBlogPost();
+	if (!isBlogPostPage) {
+		return <BlogPostItem {...props} />;
+	}
 
-  return (
-    <>
-      <BlogPostItem {...props} />
-      {(
-        <div className="docusaurus-blog-comments">
-          <Giscus
-            id="blog_comments"
-            repo="AMD-NICK/blog.amd-nick.me"
-            repoId="R_kgDOHVBfKA"
-            category="Blog comments"
-            categoryId="DIC_kwDOHVBfKM4CRnM9"
-            mapping="pathname"
-            reactionsEnabled="1"
-            emitMetadata="0"
-            inputPosition="top"
-            theme={ colorMode === "dark" ? "transparent_dark" : "light" }
-            lang={ 'ru' }
-            loading="lazy"
-          />
-        </div>
-      )}
-    </>
-  );
+	return (
+		<>
+			<BlogPostItem {...props} />
+			{(
+				<div className="docusaurus-blog-comments">
+					<Giscus
+						id="blog_comments"
+						repo="AMD-NICK/blog.amd-nick.me"
+						repoId="R_kgDOHVBfKA"
+						category="Blog comments"
+						categoryId="DIC_kwDOHVBfKM4CRnM9"
+						mapping="pathname"
+						reactionsEnabled="1"
+						emitMetadata="0"
+						inputPosition="top"
+						theme={colorMode === "dark" ? "transparent_dark" : "light"}
+						lang={'ru'}
+						loading="lazy"
+					/>
+				</div>
+			)}
+		</>
+	);
 }
